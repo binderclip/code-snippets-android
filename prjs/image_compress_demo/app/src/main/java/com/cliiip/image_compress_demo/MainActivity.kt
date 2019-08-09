e5a1b5176b80ca7fdc97b3bfa5e98c0e74f4ed63
clip <binderclip2014@gmail.com>
2019-08-09 09:35:20 +0800

    with image select

--- prjs/image_compress_demo/app/src/main/java/com/cliiip/image_compress_demo/MainActivity.kt
+++ prjs/image_compress_demo/app/src/main/java/com/cliiip/image_compress_demo/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.cliiip.image_compress_demo
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.github.chrisbanes.photoview.PhotoView
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val PICK_IMAGE_REQUEST = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val photoView = findViewById<PhotoView>(R.id.photo_view_need_select)
+        photoView.setImageResource(R.drawable.photo_c)
+    }
+
+    fun clickToSelectImageBtn(view: View) {
+        val intent = Intent()
+        intent.setType("image/*")
+        intent.setAction(Intent.ACTION_GET_CONTENT)
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK && data != null && data.data != null) {
+
+            val uri = data.data
+            val photoView = findViewById<PhotoView>(R.id.photo_view_need_select)
+            photoView.setImageURI(uri) // 可以直接设置 image uri
+
+//            try {
+//                val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+//                photoView.setImageBitmap(bitmap)
+//
+//            } catch (e: IOException) {
+//                e.printStackTrace()
+//            }
+        }
     }
 }
